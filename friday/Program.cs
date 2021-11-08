b20c3776f1e6df9027125d86258690957e9c643e
Emperormoh <muhammedbello1005@gmail.com>
2021-11-08 11:03:45 +0100

    normal Friday Class

---
+++ friday/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace friday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            char[] letters = new char[4]{ 'g', 'i', 'r', 'l' };
+            var length = letters.Length;
+            var myLetters = letters.Clone();
+            char[] newLetter = { };
+            letters.CopyTo(newLetter, 2);
+
+            int[,,] array3D = new int[3, 5, 4] {
+                { { 2, 3, 4, 5 }, { 2, 3, 4, 5 }, { 2, 3, 4, 5 }, { 2, 3, 4, 5 }, { 2, 3, 4, 5 } },
+                { { 2, 3, 4, 5 }, { 2, 3, 4, 5 }, { 2, 3, 4, 5 }, { 2, 3, 4, 5 }, { 2, 3, 4, 5 } },
+                { { 2, 3, 4, 5 }, { 2, 3, 4, 5 }, { 2, 3, 4, 5 }, { 2, 3, 4, 5 }, { 2, 3, 4, 5 } }
+            };
+
+            double[] doubleNumber = new double[4] { 1, 2, 3, 4 };
+
+            DateTime[,] dates = new DateTime[5, 5]
+            {
+                { new DateTime(2021, 11, 1), new DateTime(2021, 11, 1), new DateTime(2021, 11, 1), new DateTime(2021, 11, 1), new DateTime(2021, 11, 1) },
+                { new DateTime(2021, 11, 1), new DateTime(2021, 11, 1), new DateTime(2021, 11, 1), new DateTime(2021, 11, 1), new DateTime(2021, 11, 1) },
+                { new DateTime(2021, 11, 1), new DateTime(2021, 11, 1), new DateTime(2021, 11, 1), new DateTime(2021, 11, 1), new DateTime(2021, 11, 1) },
+                { new DateTime(2021, 11, 1), new DateTime(2021, 11, 1), new DateTime(2021, 11, 1), new DateTime(2021, 11, 1), new DateTime(2021, 11, 1) },
+                { new DateTime(2021, 11, 1), new DateTime(2021, 11, 1), new DateTime(2021, 11, 1), new DateTime(2021, 11, 1), new DateTime(2021, 11, 1) }
+
+            };
+
+
+        }
+    }
+}
